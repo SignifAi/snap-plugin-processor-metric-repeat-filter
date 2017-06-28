469a086848248397c407b3d91359c6e2819fae3c
Zach C <zcarlson@signifai.io>
2017-06-27 20:10:46 -0400

    Point to signifai fork first

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ limitations under the License.
 package main
 
 import (
-	"github.com/intelsdi-x/snap-plugin-lib-go/v1/plugin"
+	"github.com/signifai/snap-plugin-lib-go/v1/plugin"
 	"github.com/signifai/snap-plugin-processor-metric-repeat-filter/metric-repeat-filter"
 )
 
